f86b00ccdf21845e9a0155b744a6544821f8404b
ArsalanKm <arsalankarimzadeh1@gmail.com>
2022-01-01 23:41:12 +0330

    first route added

--- http-server/main.go
+++ http-server/main.go
@@ -1,6 +1,12 @@
 package main
 
+import "github.com/labstack/echo/v4"
+
+func helloHandler(c echo.Context) error{
+	return nil
+}
 
 func main(){
-	
+	app:=echo.New()
+	app.GET("/hello",helloHandler)
 }
