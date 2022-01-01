f7288aa0563948333b59f13d0cf7347414a4a17d
ArsalanKm <arsalankarimzadeh1@gmail.com>
2022-01-01 15:48:43 +0330

    Functions and Structure

--- main.go
+++ main.go
@@ -3,12 +3,34 @@ package main
 import "fmt"
 
 
-func main ()  {
-	var a []int;
-	a=make([]int, 10)
-	for index, value :=range a{
-		fmt.Printf("[%d]:%d\n",index,value)
+type Student struct{
+	Name string;
+	Family string;
+	Average float64;
+}
+
+func Hello(count int,s Student){
+for i := 0; i < count; i++ {
+	fmt.Printf("Hello %s %s \n",s.Name,s.Family)
+}
+}
+func MultipleReturn(v int) (int ,int){
+	return v-1 , v+1
+}
+func IncreaseAverage(i int,s Student) Student{
+	s.Average+=float64(i);
+	return s;
+}
+
+func main(){
+	s :=Student{
+		Name: "arsalan",
+		Family: "Karimzad",
+		Average: 15.4,
 	}
-	
+	fmt.Println(s)
+	Hello(5,s);
+	s=IncreaseAverage(1,s)
+	fmt.Println(s)
 
 }
