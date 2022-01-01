1358a0068ac3da71e8e3a446961b3e29a008f508
ArsalanKm <arsalankarimzadeh1@gmail.com>
2022-01-01 16:02:50 +0330

    interfaces added

--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import "fmt"
 
 
+type Helloer interface{
+	Hello(count int)
+}
+
+type H1 interface{
+	Hello(count int)
+}
+
 type Student struct{
 	Name string;
 	Family string;
 	Average float64;
 }
 
-func Hello(count int,s Student){
+func (s Student) Hello(count int){
 for i := 0; i < count; i++ {
 	fmt.Printf("Hello %s %s \n",s.Name,s.Family)
 }
@@ -17,7 +25,7 @@ for i := 0; i < count; i++ {
 func MultipleReturn(v int) (int ,int){
 	return v-1 , v+1
 }
-func IncreaseAverage(i int,s Student) Student{
+func (s Student)IncreaseAverage(i int,) Student{
 	s.Average+=float64(i);
 	return s;
 }
@@ -29,8 +37,10 @@ func main(){
 		Average: 15.4,
 	}
 	fmt.Println(s)
-	Hello(5,s);
-	s=IncreaseAverage(1,s)
+	s.Hello(5);
+	s=s.IncreaseAverage(1)
 	fmt.Println(s)
+	var h Helloer = s
+	h.Hello(10)
 
 }
