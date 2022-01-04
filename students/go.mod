5aa4f2efec04d1659fbe6a1fff6d833ad239838e
ArsalanKm <arsalankarimzadeh1@gmail.com>
2022-01-04 17:48:44 +0330

    go mod tidy added and some error fixed

--- students/go.mod
+++ students/go.mod
@@ -4,12 +4,8 @@ go 1.17
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/go-ozzo/ozzo-validation/v4 v4.3.0 // indirect
-	github.com/gofiber/fiber/v2 v2.24.0 // indirect
-	github.com/klauspost/compress v1.13.6 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.31.0 // indirect
-	github.com/valyala/tcplisten v1.0.0 // indirect
-	go.mongodb.org/mongo-driver v1.8.1 // indirect
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
+	github.com/gofiber/fiber/v2 v2.24.0
+	go.mongodb.org/mongo-driver v1.8.1
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 )
--- students/internal/db/db.go
+++ students/internal/db/db.go
@@ -13,26 +13,24 @@ import (
 func new(cfg Config) (*mongo.Database,error){
 	opts:=options.Client()
 	opts.ApplyURI(cfg.URL)
-
+	
 	client,err:=mongo.NewClient(opts)
 	if err!=nil{
 		return nil,fmt.Errorf("db New client errir %s",err)
 	}
-{
 
-	ctx ,done‌:=context.WithTimeout(context.Background(),cfg.ConnectionTimeout)
+
+	ctx , done:=context.WithTimeout(context.Background(),cfg.ConnectionTimeout)
 	defer done()
-	if err := client.Connect(ctx):err !=nil{
+	if err := client.Connect(ctx);err !=nil{
 		return nil,fmt.Errorf("db connection error %w",err)
 	}
-}
-
 
 {
 
-	ctx ,done‌:=context.WithTimeout(context.Background(),cfg.ConnectionTimeout)
+	ctx ,done:=context.WithTimeout(context.Background(),cfg.ConnectionTimeout)
 	defer done()
-	if err := client.Ping(ctx,readpref.Primary()):err !=nil{
+	if err := client.Ping(ctx,readpref.Primary());err !=nil{
 		return nil,fmt.Errorf("db connection error %w",err)
 	}
 }
